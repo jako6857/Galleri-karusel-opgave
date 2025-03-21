618cb97f5f6c85f2e0638228cad0c7e0a8ae53bd
jako6857 <82064889+jako6857@users.noreply.github.com>
2025-03-21 11:12:20 +0100

    img slider (partly complete)

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,38 @@
 // Din cool code here
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  slideIndex += n;
+  showSlides(slideIndex);
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides-fade");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex + 1].className += " active";
+}
